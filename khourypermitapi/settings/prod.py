576a578ffdb84d048e86654ea78991014cc9a725
PrimarisEquilibrium <incorrectclock1@gmail.com>
2023-02-05 11:30:58 -0500

    CORS All Origins

--- khourypermitapi/settings/prod.py
+++ khourypermitapi/settings/prod.py
@@ -10,17 +10,19 @@ ALLOWED_HOSTS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://khourypermit-api.herokuapp.com"
+    "https://khourypermit-api.herokuapp.com",
 ]
 
-CORS_ALLOW_CREDENTIALS = True
+# CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_HEADERS = ['*']
+# CORS_ALLOW_HEADERS = ['*']
 
-CORS_ALLOWED_ORIGINS = [
-    "https://khourybuildingpermits.com",
-    "http://localhost:3000",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "https://khourybuildingpermits.com",
+#     "http://localhost:3000",
+# ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Production Apps
 PROD_APPS = [
