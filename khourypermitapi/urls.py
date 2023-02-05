707a6f801b8374fb6a4e20f514f533c8047a3149
PrimarisEquilibrium <incorrectclock1@gmail.com>
2023-02-05 10:06:06 -0500

    Removed Index View

--- khourypermitapi/urls.py
+++ khourypermitapi/urls.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from views import index
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include("backend.urls")),
-    path('', index),
     
     path('__debug__/', include('debug_toolbar.urls'))
 ]
--- khourypermitapi/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Khoury Design and Permit Drawings API")
