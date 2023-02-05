3612bf4c1e2f531c2256fe1dcab724996969d545
PrimarisEquilibrium <incorrectclock1@gmail.com>
2023-02-05 09:44:53 -0500

    Allowed_host Error Resolved

--- khourypermitapi/settings/prod.py
+++ khourypermitapi/settings/prod.py
@@ -5,7 +5,7 @@ import dj_database_url
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["khourypermitapi.herokuapp.com"]
+ALLOWED_HOSTS = ["khourypermit-api.herokuapp.com"]
 
 CORS_ALLOWED_ORIGINS = [
     "khourybuildingpermits.com"
